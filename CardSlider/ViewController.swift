ff1b28c7829c09eb32c82083cf7e4ef65998669f
Saoud Rizwan <fcmiapps@gmail.com>
2017-02-27 12:54:54 -0800

    Initial Commit

---
+++ CardSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardSlider
+//
+//  Created by Saoud Rizwan on 2/27/17.
+//  Copyright © 2017 Saoud Rizwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
